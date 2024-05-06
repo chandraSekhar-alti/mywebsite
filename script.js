935250a3e41449f493033f0acad4c3b87c10074b
chandraSekhar-alti <althichandu123@gmail.com>
2024-05-06 14:33:59 +0530

    added emailjs functionality

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+function sendEmail(){
+    console.log("function calling!!!!")
+    let params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+    console.log("params :- ",params)
 
-window.scroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+    emailjs.send("service_0d46s1k","template_pxbks3l",params).then(alert("Your email has been sent !!!!!!"));
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*='+id+ ']').classList.add('active')
-            })
-        }
-    })
+    console.log("function ended !!!!!!")
 }
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+
+
+// let menuIcon = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+// let sections = document.querySelectorAll("section");
+// let navLinks = document.querySelectorAll("header nav a");
+
+// window.onscroll = () => {
+//     sections.forEach((sec) => {
+//       let top = window.scrollY;
+//       let offset = sec.offsetTop - 150;
+//       let height = sec.offsetHeight;
+//       let id = sec.getAttribute("id");
+  
+//       if (top >= offset && top < offset + height) {
+//         navLinks.forEach((link) => {
+//           link.classList.remove("active");
+//         });
+//         document
+//           .querySelector("header nav a[href*=" + id + "]")
+//           .classList.add("active");
+//       }
+//     });
+//   };
+  
+
+//   menuIcon.onclick = () => {
+//     menuIcon.classList.toggle("bx-x");
+//     navbar.classList.toggle("active");
+//   };
+
+
+
+
+
